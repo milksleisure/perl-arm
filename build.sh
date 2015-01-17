937b35b0baa284fed4189a4f09bbe1dfec8317b5
B.R. Wu <killuaken@gmail.com>
2015-01-17 16:16:00 +0800

    Update the build script
    - This version works on linaro aarch64 minimal image

--- build.sh
+++ build.sh
@@ -1,56 +1,38 @@
 #!/bin/bash
-ANDROID_NDK=/opt/android-ndk
-ARCH=arm-linux-androideabi
-TOOLCHAIN=/tmp/standalone-$ARCH
-SYSROOT=$TOOLCHAIN/sysroot
-TARGET=/data/local/tmp/perl
-PATH=$ANDROID_NDK/toolchains/$ARCH-4.8/prebuilt/linux-x86_64/bin:$PATH
-
-function toolchain {
-    $ANDROID_NDK/build/tools/make-standalone-toolchain.sh \
-        --platform=android-9 \
-        --install-dir=$TOOLCHAIN \
-        --system=linux-x86_64 \
-        --toolchain=$ARCH-4.8
-}
+CROSSTOOL=/opt/gcc-linaro-4.9-2014.11-x86_64_aarch64-linux-gnu
+PATH=${CROSSTOOL}/bin/:$PATH
 
 function config {
-    ./Configure \
-        -des \
-        -Dusedevel \
-        -Dusecrosscompile \
-        -Dtargetrun=adb \
-        -Dcc=arm-linux-androideabi-gcc \
-        -Dsysroot=$SYSROOT \
-        -Dtargetdir=$TARGET \
-        -Dtargethost=$DEVICE
+    ./configure \
+        --target=aarch64-linux-gnu \
+        -Dusethreads -Duse64bitall \
+        -Doptimize=-O2 \
+        --all-static \
+        --no-dynaloader
 }
 
 function build {
     make -j7
 }
 
-function test_install {
-    make test
-}
-
 function install {
-    make install
+    mkdir -p ./out
+    make DESTDIR=./out install
 }
 
 function main {
     if [ "$1" == "all" ]; then
-        toolchain
-    elif [ "$1" == "toolchain" ]; then
-        toolchain
+        config
+        build
+        install
     elif [ "$1" == "config" ]; then
         config
     elif [ "$1" == "build" ]; then
         build
-    elif [ "$1" == "test_install" ]; then
-        test_install
+    elif [ "$1" == "install" ]; then
+        install
     else
-        echo "build.sh [all | toolchain | config | build | test_install]"
+        echo "build.sh [all | toolchain | config | build | install]"
     fi
 }
 
